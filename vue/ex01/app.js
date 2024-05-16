28144e8c3c4fcfd4cec3878285e3505ee3813547
Kyoungsic Kim <cukehater@gmail.com>
2024-05-16 16:51:31 +0900

    vue: udemy 강의 학습

---
+++ vue/ex01/app.js
@@ -0,0 +1,14 @@
+Vue.createApp({
+  data: function () {
+    return {
+      goals: [],
+      enteredValue: ''
+    }
+  },
+  methods: {
+    addGoal: function () {
+      this.goals.push(this.enteredValue)
+      this.enteredValue = ''
+    }
+  }
+}).mount('#app')
